67761f471ee625b679a6d637322466c9b2bedcb6
Omar Khan <omarkhan01@cs.uchicago.edu>
2022-02-17 10:36:58 -0600

    barebones question suggestions

---
+++ suggestions.py
@@ -0,0 +1,15 @@
+# Winter '22 - CS122 Group Project - [insert group name here]
+
+# Code for mapping uninterpretable questions to preset questions [DRAFT]
+
+# Omar Khan, Vidyut Baradwaj, Max Huisman
+
+Q1 = "What are the " + course.prerequisites + " for this course?"
+q1map = ["prerequisite", "prerequisites", "prereq", "required", "requirements", "need", "sequence", "sequences"]
+
+Q2 = "Does " + course.full_string + " count as an elective for" + program
+q2map = ["elective", "count", ""]
+
+Q3 = "How are the " + course.prof + "'s reviews?"
+q3map = ["difficulty", "hours", "work", "hard", "easy"]
+
