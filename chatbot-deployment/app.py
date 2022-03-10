1401daf51deedfb77b12228c18c901ec975833a5
Vidyut Arun Baradwaj <vidyut@cs.uchicago.edu>
2022-03-10 13:33:07 -0600

    almost done integrating front end

--- chatbot-deployment/app.py
+++ chatbot-deployment/app.py
@@ -1,23 +1,24 @@
 import flask
 from flask import Flask, render_template, request, jsonify
 #from flask_cors import CORS
-
+from sel import get_course_eval
 
 app = Flask(__name__)
 #CORS(app)
 
-@app.route("/")
+@app.get("/")
 
 def index_get():
     return render_template("base.html") 
 
-@app.route("/predict", methods=['POST'])
+@app.post("/predict")
 def predict():
     # get user input
     #response = get_response(user_input)
-    response = "Sorry, I can't do anything right now lol"  # get bot output from back-end
+    text = request.get_json().get("message")
+    # error check --> if text is valid
+    response = get_course_eval(text)  # get bot output from back-end
     message = {"answer": response}
-    print(message)
     return jsonify(message)
 
 
